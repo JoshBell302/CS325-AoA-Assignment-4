2db92907c1ccd3bcd0e99979a401e1f21051a318
Josh Bell <bellj3@oregonstate.edu>
2021-05-16 20:24:40 -0700

    Code now finds the total weight of an MST
    Code now fills an array, called "graph," with the distances between each vertex. This information is then sent through the Prim's Algorithm to find the total weight of the minimum spanning tree!

--- mst.cpp
+++ mst.cpp
@@ -3,17 +3,67 @@
 #include <string>
 #include <sstream>
 #include <cstring>
+#include <bits/stdc++.h>
+#include <limits>
 
 #define _CRT_SECURE_NO_WARNINGS
 #define TEST_CASES 10
 #define NUM_OF_COORDINATES 20
 #define X_AND_Y 2
+#define COORDINATE_MAX 25
+
+int distance(int x1, int y1, int x2, int y2) {
+    return round(sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)));
+}
+
+int prims(int graph[NUM_OF_COORDINATES][NUM_OF_COORDINATES]) {
+    // Initialize variables
+    int x = 0;
+    int y = 0;
+    int numOfEdge = 0;
+    int totalWeight = 0;
+    int selected[NUM_OF_COORDINATES];
+    
+    // Set all values of selected my false
+    memset(selected, false, sizeof(selected));
+    // Set the first vertex to true
+    selected[0] = true;
+    
+    // Loop to find the edges 
+    while (numOfEdge < NUM_OF_COORDINATES - 1) {
+        // Reset all variables
+        int min = std::numeric_limits<int>::max();
+        x = 0;
+        y = 0;
+
+        // Loops through the graph array to find the best edge for the minimum spanning tree
+        for (int i = 0; i < NUM_OF_COORDINATES; i++) {
+            if (selected[i]) {
+            for (int j = 0; j < NUM_OF_COORDINATES; j++) {
+                if (!selected[j] && graph[i][j]) {
+                    if (min > graph[i][j]) {
+                        min = graph[i][j];
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+        }
+    }
+
+    // Update the variables
+    totalWeight = totalWeight + graph[x][y];
+    selected[y] = true;
+    numOfEdge++;
+  }
+  return totalWeight;
+}
 
 int main() {
 	// Initialize variables
     int coordinates[TEST_CASES][NUM_OF_COORDINATES][X_AND_Y] = { 0 };
     int numOfTests = 0;
-    int numOfVerts = 0;
+    int numOfVerts[TEST_CASES] = { 0 };
     int value = 0;
     bool coor = false;
 	std::string str;
@@ -28,10 +78,10 @@ int main() {
         // This loop reads each tests
         for (int i = 0; i < numOfTests; i++) {
             std::getline(file,str);
-            numOfVerts = std::stoi(str);
-            std::cout << "The number of vertices is: " << numOfVerts << std::endl;
+            numOfVerts[i] = std::stoi(str);
+            std::cout << "The number of vertices is: " << numOfVerts[i] << std::endl;
             // This loops the number of times according to the amount of vertices, and reads the coordinates afterwards
-            for (int j = 0; j < numOfVerts; j++) {
+            for (int j = 0; j < numOfVerts[i]; j++) {
                 std::getline(file,str);
                 std::stringstream stream(str);
                 while(1) {
@@ -48,5 +98,23 @@ int main() {
 	}
 	else
 		std::cout << "Could not load the file";
+
+    // We now begin taking the data and spliting it into a graph matrix that harbors the distance between each vertex
+    int graph[NUM_OF_COORDINATES][NUM_OF_COORDINATES] = { 0 };
+
+    for (int i = 0; i < numOfTests; i++) {
+        std:: cout << "Graph creation Test: " << i + 1 << std::endl;
+        // Reset the graph array to all zeros
+        std::memset(graph, 0, sizeof graph);
+        for (int j = 0; j < numOfVerts[i]; j++) {
+            for (int k = 0; k < numOfVerts[i]; k++) {
+                // Calculate the distance between all points and store them into graphs
+                graph[j][k] = distance(coordinates[i][j][0],coordinates[i][j][1], coordinates[i][k][0], coordinates[i][k][1]);
+                std::cout << "Graph point " << j << " " << k << " = " << graph[j][k] << std::endl;
+            }
+        } 
+        std::cout << "The total weight is: " << prims(graph) << std::endl;
+        std::cout << std::endl;
+    }
 	return 0;
 }
